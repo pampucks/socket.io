d02ea844b78504ab9ef4a9058fcc9ef0253753a2
Fahmi Ardiansyah <pampucks@gmail.com>
2024-05-28 18:52:47 +0700

    tambah update download

--- examples/private-messaging/server/index.js
+++ examples/private-messaging/server/index.js
@@ -1,7 +1,7 @@
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "http://192.168.1.110:8080",
   },
 });
 
@@ -16,6 +16,7 @@ const messageStore = new InMemoryMessageStore();
 
 io.use((socket, next) => {
   const sessionID = socket.handshake.auth.sessionID;
+  // console.log(sessionID);
   if (sessionID) {
     const session = sessionStore.findSession(sessionID);
     if (session) {
@@ -36,6 +37,7 @@ io.use((socket, next) => {
 });
 
 io.on("connection", (socket) => {
+  console.log(socket.userID);
   // persist session
   sessionStore.saveSession(socket.sessionID, {
     userID: socket.userID,
@@ -53,7 +55,7 @@ io.on("connection", (socket) => {
   socket.join(socket.userID);
 
   // fetch existing users
-  const users = [];
+  let users = [];
   const messagesPerUser = new Map();
   messageStore.findMessagesForUser(socket.userID).forEach((message) => {
     const { from, to } = message;
@@ -83,15 +85,56 @@ io.on("connection", (socket) => {
   });
 
   // forward the private message to the right recipient (and to other tabs of the sender)
-  socket.on("private message", ({ content, to }) => {
+  socket.on("private message", ({ from, content, to }) => {
     const message = {
       content,
-      from: socket.userID,
+      // from: socket.userID,
+      from,
       to,
     };
-    socket.to(to).to(socket.userID).emit("private message", message);
-    messageStore.saveMessage(message);
+    if (message.to == "socketserver") {
+      console.log("from: " + message.from + "\n" + "to: " + message.to + "\n" + "content: " + message.content + "\n")
+    }
+    // socket.to(to).to(socket.userID).emit("private message", message);
+    // messageStore.saveMessage(message);
   });
+
+  socket.on("update download", ({duid, zipname, wktdownload, to}) => {
+    const message = {
+      duid,
+      zipname,
+      wktdownload,
+      to,
+    }
+    if (message.duid != "" && message.to != "" && message.zipname != "" && message.wktdownload != "") {
+      const responseMsg = {
+        statusMsg: "success",
+        code: 200
+      }
+      if (message.to == "socketserver"){
+        console.log("duid: " + message.duid + "\n" + "zipname: " + message.zipname + "\n" + "wktdownload: " + message.wktdownload + "\n")
+        socket.emit("response", responseMsg)
+      }
+    }
+  })
+
+  socket.on("update content", ({message, to, from}) => {
+    const requestContent = {
+      message,
+      to,
+      from
+    }
+    const contentAMG = {
+      sesiJadwal: "G4T6G34G4W",
+      namaKonten: "s20240502154241.mp4",
+      sizeKonten: "3137581",
+      urutanKonten: 3
+    }
+    if (to == "socketserver") {
+      console.log("message: " + requestContent.message + "\n" + "to: " + requestContent.to + "\n" + "from: " + requestContent.from + "\n")
+      socket.emit("update content", contentAMG)
+    }
+  })
 
   // notify users upon disconnection
   socket.on("disconnect", async () => {
@@ -106,6 +149,16 @@ io.on("connection", (socket) => {
         username: socket.username,
         connected: false,
       });
+      users = [];
+      sessionStore.findAllSessions().forEach((session) => {
+        users.push({
+          userID: session.userID,
+          username: session.username,
+          connected: session.connected,
+          messages: messagesPerUser.get(session.userID) || [],
+        });
+      });
+      socket.broadcast.emit("users", users);
     }
   });
 });
@@ -113,5 +166,5 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 
 httpServer.listen(PORT, () =>
-  console.log(`server listening at http://localhost:${PORT}`)
+  console.log(`server listening at http://192.168.1.110:${PORT}`)
 );
